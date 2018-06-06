b0ad333368f5ce207db714c29a7fb792df595cc9
Derek Gilbert <derekgilbert@controlpad.local>
2018-06-05 23:34:29 -0600

    adding server fuctionality

--- index.js
+++ index.js
@@ -6,12 +6,28 @@
 
 // dependencies
 const http = require("http");
+const url = require("url");
 
 // server object
 const server = http.createServer(function(req, res) {
+  // get url
+  var pareseUrl = url.parse(req.url, true);
+  // console.log("Parsed Url:", pareseUrl);
+
+  // get path
+  var path = pareseUrl.pathname;
+  console.log("Parsed Path:", path);
+  var trimmedPath = path.replace(/^\/+|\/+$/g, "");
+  console.log("trimmedPath:", trimmedPath);
+
+  // send response
   res.end("Hello World\n");
+
+  // log path
+  console.log("request has been recieved: " + trimmedPath);
 });
 
+// starting server
 server.listen(3000, function() {
-  console.log("the server is listening on the port 3000");
+  console.log("the server is listening on the port 3000\n...");
 });
