3b4a83e9c8db832f7584a0e349cbdb4f1496f5ca
Derek Gilbert <derekgilbert@controlpad.local>
2018-06-05 22:52:21 -0600

    server stuff

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 /* 
  *
  * 
+ * 
  */
+
+// dependencies
 const http = require("http");
+
+// server object
+const server = http.createServer(function(req, res) {
+  res.end("Hello World\n");
+});
+
+server.listen(3000, function() {
+  console.log("the server is listening on the port 3000");
+});
