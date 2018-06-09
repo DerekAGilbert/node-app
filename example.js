36df755a576d1a80173127142caa02f7d9802174
Derek Gilbert <derekgilbert@controlpad.local>
2018-06-08 20:28:33 -0600

    stuff

---
+++ example.js
@@ -0,0 +1,16 @@
+import data from "example.json";
+
+function spliter(str, spliter) {
+    var newstring = str.split(spliter)
+    return newstring
+}
+function mapper() {
+    
+}
+
+var space = "-"
+var dot = "."
+var boom = "boom.boom.pow"
+// var boom = "boom boom pow"
+var newArray = spliter(boom, dot).map(x => x += " hello")
+console.log(newArray);
--- index.js
+++ index.js
@@ -1,33 +1,84 @@
 /* 
  *
- * 
- * 
  */
-
 // dependencies
 const http = require("http");
 const url = require("url");
-
+const StringDecoder = require("string_decoder").StringDecoder;
 // server object
 const server = http.createServer(function(req, res) {
   // get url
-  var pareseUrl = url.parse(req.url, true);
-  // console.log("Parsed Url:", pareseUrl);
+  var parsedUrl = url.parse(req.url, true);
 
   // get path
-  var path = pareseUrl.pathname;
-  console.log("Parsed Path:", path);
+  var path = parsedUrl.pathname;
   var trimmedPath = path.replace(/^\/+|\/+$/g, "");
-  console.log("trimmedPath:", trimmedPath);
 
-  // send response
-  res.end("Hello World\n");
+  // get querey string as an object
+  var queryStringObject = parsedUrl.query;
 
-  // log path
-  console.log("request has been recieved: " + trimmedPath);
+  // get http method
+  var method = req.method.toLocaleLowerCase();
+
+  //get headers as an object
+  var headers = req.headers;
+
+  //get payload if there is one
+  var decoder = new StringDecoder("utf-8");
+  var buffer = "";
+  req.on("data", function(data) {
+    buffer += decoder.write(data);
+  });
+  req.on("end", function() {
+    buffer += decoder.end();
+
+    // choose hanlder request should go to if no found, use not found handler
+    var chosenHandler = typeof(router[trimmedPath] !== 'undefined' ? router[trimmedPath] : handlers.notFound)
+    // contruct data object to send to handler
+    var data = {
+      "trimmedPath" : trimmedPath,
+      "queryStringObject": queryStringObject,
+      "method": method,
+      "headers": headers,
+      "payload": payload
+    }
+
+    // route the request specified in the route
+    chosenHandler(data,function(statusCode,payload){
+      
+    })
+    // send response
+    res.end("Hello World\n");
+
+    // log path
+    console.log('request was recieved with this payload: ', buffer)
+    console.log("headers:", headers);
+    console.log("request:" + trimmedPath + "method:" + method + "query string parameters:", queryStringObject);
+  });
 });
 
 // starting server
 server.listen(3000, function() {
   console.log("the server is listening on the port 3000\n...");
 });
+//
+// define out handlers
+var handlers = {};
+// sample handler
+
+handlers.sample = function(data,callback){
+// http satus code, payload should be an object
+callback(406,{"name": "derek"})
+}
+
+// not found handler
+
+handlers.notFound = function(data,callback){
+callback(404)
+}
+
+// define a rwuest router
+var router = {
+  'sample': handlers.sample
+}
+
